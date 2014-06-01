b7dd6727934fea9f757f2e4861b54b4d042cf9e5
MattBubernak <soccerfreak24@gmail.com>
2014-06-01 16:54:48 -0600

    completed team selector page

---
+++ SportStatTracker/SportStatTracker/Views/TeamSelectionPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SportStatTracker.Views
+{
+    public partial class TeamSelectionPage : PhoneApplicationPage
+    {
+        public TeamSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Team(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
