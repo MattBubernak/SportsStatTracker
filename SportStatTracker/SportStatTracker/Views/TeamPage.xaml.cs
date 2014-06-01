3b6674539e1c5437fc60f9839f12dfeebd24a40a
MattBubernak <soccerfreak24@gmail.com>
2014-06-01 17:31:31 -0600

    added panorama button bar for team page

--- SportStatTracker/SportStatTracker/Views/TeamPage.xaml.cs
+++ SportStatTracker/SportStatTracker/Views/TeamPage.xaml.cs
@@ -16,5 +16,59 @@ namespace SportStatTracker.Views
         {
             InitializeComponent();
         }
+
+        private void PanoramaSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int currentPanoramaIndex = PanoramaControl.SelectedIndex;
+            UpdatePanoramaAppBar(currentPanoramaIndex);
+        }
+
+        private void add_player(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_game(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdatePanoramaAppBar(int index)
+        {
+
+            //create a new application bar
+            ApplicationBar = new ApplicationBar();
+
+            switch (index)
+            {
+                case 0:
+                    //create a button for new player 
+                    ApplicationBarIconButton newGame = new ApplicationBarIconButton();
+                    //populate the button information 
+                    newGame.Text = "Add Game";
+                    newGame.IconUri = new Uri("/Images/add.png", UriKind.Relative);
+                    newGame.Click += new EventHandler(add_game);
+                    //add the button to the application bar 
+                    ApplicationBar.Buttons.Add(newGame);
+                    break;
+                case 1:
+                    //create a button for new player 
+                    ApplicationBarIconButton newPlayer = new ApplicationBarIconButton();
+                    //populate the button information 
+                    newPlayer.Text = "Add Player";
+                    newPlayer.IconUri = new Uri("/Images/add.png", UriKind.Relative);
+                    newPlayer.Click += new EventHandler(add_player);
+                    //add the button to the application bar 
+                    ApplicationBar.Buttons.Add(newPlayer);
+
+                    break;
+                case 2:
+
+
+                    break;
+            }
+        }
+
+
     }
 }
