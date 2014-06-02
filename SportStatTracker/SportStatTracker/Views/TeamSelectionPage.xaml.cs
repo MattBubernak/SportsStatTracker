54987db28861192534d0b53ca39d113d77adcc6a
MattBubernak <soccerfreak24@gmail.com>
2014-06-01 18:23:32 -0600

    changed the teamselectionpage to panorama and added about

--- SportStatTracker/SportStatTracker/Views/TeamSelectionPage.xaml.cs
+++ SportStatTracker/SportStatTracker/Views/TeamSelectionPage.xaml.cs
@@ -15,6 +15,7 @@ namespace SportStatTracker.Views
         public TeamSelectionPage()
         {
             InitializeComponent();
+            UpdatePanoramaAppBar(0);
         }
 
         private void Add_Team(object sender, EventArgs e)
@@ -22,5 +23,39 @@ namespace SportStatTracker.Views
             NavigationService.Navigate(new Uri("/Views/AddTeam.xaml", UriKind.Relative));
 
         }
+
+        private void PanoramaSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int currentPanoramaIndex = PanoramaControl.SelectedIndex;
+            UpdatePanoramaAppBar(currentPanoramaIndex);
+        }
+
+        private void UpdatePanoramaAppBar(int index)
+        {
+
+            //create a new application bar
+            ApplicationBar = new ApplicationBar();
+
+            switch (index)
+            {
+                case 0:
+                    //create a button for new player 
+                    ApplicationBarIconButton newGame = new ApplicationBarIconButton();
+                    //populate the button information 
+                    newGame.Text = "Add Game";
+                    newGame.IconUri = new Uri("/Images/add.png", UriKind.Relative);
+                    newGame.Click += new EventHandler(Add_Team);
+                    //add the button to the application bar 
+                    ApplicationBar.Buttons.Add(newGame);
+                    break;
+                case 1:
+
+                    break;
+
+            }
+        }
+
+
+
     }
 }
