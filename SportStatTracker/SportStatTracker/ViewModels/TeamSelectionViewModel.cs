ee5a8a054f91b7876da2f8f0ab1402b709404551
MattBubernak <soccerfreak24@gmail.com>
2014-06-02 21:26:49 -0600

    added a collection of teams and an empty populate_teams function

--- SportStatTracker/SportStatTracker/ViewModels/TeamSelectionViewModel.cs
+++ SportStatTracker/SportStatTracker/ViewModels/TeamSelectionViewModel.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using SportStatTracker.Models;
 
 namespace SportStatTracker.ViewModels
 {
     class TeamSelectionViewModel
     {
         private static TeamSelectionViewModel _instance = null;
+        public ObservableCollection<Models.DataBaseContext.Team> Teams { get; set; }
 
         private TeamSelectionViewModel()
         {
-
+            //should populate the teams from the DB 
+            populate_teams(); 
         }
 
         public TeamSelectionViewModel getInstance()
@@ -22,6 +28,12 @@ namespace SportStatTracker.ViewModels
                 _instance = new TeamSelectionViewModel();
             }
             return _instance;
+        }
+
+        //populates Teams with the teams from the DB 
+        public populate_teams()
+        {
+
         }
     }
 }
